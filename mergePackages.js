1c4be4f1b8793ccf26a42cd0e3b93354daadf166
Duarte David <deltaduartedavid@gmail.com>
2017-08-13 18:18:10 +0200

    Remove scripts from published package.json

--- mergePackages.js
+++ mergePackages.js
@@ -3,4 +3,4 @@ const { readFileSync, writeFileSync } = require('fs');
 const currentPackage = JSON.parse(readFileSync('package.json'));
 const CSSLSPackage = JSON.parse(readFileSync('vscode-css-languageserver/package.json'));
 
-writeFileSync('dist/package.json', JSON.stringify(Object.assign({}, CSSLSPackage, currentPackage), null, 2));
+writeFileSync('dist/package.json', JSON.stringify(Object.assign(CSSLSPackage, currentPackage, { scripts: {} }), null, 2));
