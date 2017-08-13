dcee9e81ce127743c2bf2fd12898237b99823c28
Duarte David <deltaduartedavid@gmail.com>
2017-08-13 17:46:43 +0200

    Package vscode-css-languageserver into binary

---
+++ mergePackages.js
@@ -0,0 +1,6 @@
+const { readFileSync, writeFileSync } = require('fs');
+
+const currentPackage = JSON.parse(readFileSync('package.json'));
+const CSSLSPackage = JSON.parse(readFileSync('vscode-css-languageserver/package.json'));
+
+writeFileSync('dist/package.json', JSON.stringify(Object.assign({}, CSSLSPackage, currentPackage), null, 2));
